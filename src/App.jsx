534c8567c3e75d5d090cd941ad32c57cf7a65ca2
Nishant Siwakoti <nsiwakoti18@gmail.com>
2024-08-01 22:08:20 +0545

    Deployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import AllRoutes from "./Routes/AllRoutes";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <AllRoutes />
       <Footer />
+      <Analytics />
     </>
   );
 }
